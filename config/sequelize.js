fef0c26356d8bc7cd279524aa43e90c5e65f434f
Yunya Hsu <yunyahsu110@gmail.com>
2022-10-31 16:54:52 +0800

    feat: initialize sequelize

---
+++ config/sequelize.js
@@ -0,0 +1,19 @@
+const path = require('path')
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
+
+module.exports = {
+  "development": {
+    "host": process.env.DB_DEV_HOST,
+    "username": process.env.DB_DEV_USER,
+    "password": process.env.DB_DEV_PASSWORD,
+    "database": process.env.DB_DEV_DATABASENAME,
+    "dialect": "mysql"
+  },
+  "production": {
+    "host": process.env.DB_PROD_HOST,
+    "username": process.env.DB_PROD_HOST,
+    "password": process.env.DB_PROD_PASSWORD,
+    "database": process.env.DB_PROD_DATABASENAME,
+    "dialect": "mysql"
+  }
+}
