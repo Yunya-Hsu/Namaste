40eed3c4222d5cacbc02f806c0956c53a8267d35
Yunya Hsu <yunyahsu110@gmail.com>
2022-11-02 11:26:05 +0800

    feat: add multer & post studio API

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ require('dotenv').config()
 const http = require('http')
 const express = require('express')
 const app = express()
+const path = require('path')
 const cors = require('cors')
 const { engine } = require('express-handlebars')
+const session = require('express-session')
+const flash = require('connect-flash')
+
 const server = http.createServer(app)
 const io = require('socket.io')(server, {
   cors: {
@@ -14,9 +18,17 @@ const port = process.env.SERVER_PORT || 3000
 const router = require('./routes/index')
 
 app.use(express.static('public'))
+app.use('/images', express.static(path.join(__dirname, 'images'))) // 讓外部檔案也可以使用 images 裡面的檔案
+
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(cors())
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  saveUninitialized: true,
+  resave: false
+}))
+app.use(flash())
 
 app.engine('handlebars', engine())
 app.set('view engine', 'handlebars')
--- controllers/admin_controller.js
+++ controllers/admin_controller.js
@@ -1,15 +1,67 @@
+/* eslint-disable camelcase */
+const moment = require('moment')
+
+// models
+const Admin = require('../models/admin')
+
+
 const studio = {
   name: 'Namaste',
   backgroundColor: '#F8F6F5',
   logo: '/namaste/namaste_logo.png'
 }
+const requirementOfCreateStudio = ['name', 'subdomain', 'manager', 'address', 'tappay_app_key', 'tappay_partner_key', 'tappay_id']
+
+
 
 const renderCreateStudioPage = (req, res) => {
-  res.render('super_admin/studio', { studio })
+  const input = req.session.flash && req.session.flash.createStudioInput ? req.session.flash.createStudioInput[0] : null
+  const errorMessage = req.session.flash && req.session.flash.errorMessage ? req.session.flash.errorMessage[0] : null
+  const successMessage = req.session.flash && req.session.flash.successMessage ? req.session.flash.successMessage[0] : null
+  delete req.session.flash
+
+  return res.render('super_admin/studio', { studio, input, errorMessage, successMessage })
 }
 
 const createStudio = async (req, res) => {
+  // 檢查前端資料，若不足則擋下
+  if (!req.files.logo || !requirementOfCreateStudio.every(e => req.body[e] !== undefined)) {
+    req.flash('createStudioInput', req.body)
+    req.flash('errorMessage', { errorMessage: 'missing information' })
+    return res.redirect('/admin/studio')
+  }
 
+  // check if subdomain unique
+  const checkSubdomainResult = await Admin.checkSubdomain(req.body.subdomain)
+  if (checkSubdomainResult.length > 0) {
+    req.flash('createStudioInput', req.body)
+    req.flash('errorMessage', { errorMessage: 'subdomain is duplicate' })
+    return res.redirect('/admin/studio')
+  }
+
+  // FIXME: validate subdomain formate
+
+  // check if manager account is validate
+  const managerRoleId = await Admin.findRoleIdByEmail(req.body.manager)
+  if (managerRoleId === null) {
+    req.flash('createStudioInput', req.body)
+    req.flash('errorMessage', { errorMessage: `${req.body.manager} does not exist` })
+    return res.redirect('/admin/studio')
+  } else if (managerRoleId !== 1) {
+    req.flash('createStudioInput', req.body)
+    req.flash('errorMessage', { errorMessage: 'this user is not boss' })
+    return res.redirect('/admin/studio')
+  }
+
+  // insert into db
+  const { name, introduction_title, introduction_detail, subdomain, address, address_description, phone, tappay_app_key, tappay_partner_key, tappay_id } = req.body
+  const logo = req.files.logo[0].path
+  const introduction_photo = req.files.introduction_photo ? req.files.introduction_photo[0].path : null
+  const currentTime = moment().format('YYYY-MM-DD HH:mm:ss')
+
+  await Admin.createStudio(name, introduction_title, introduction_detail, subdomain, managerRoleId, address, address_description, phone, tappay_app_key, tappay_partner_key, tappay_id, logo, introduction_photo, currentTime, currentTime)
+  req.flash('successMessage', { successMessage: 'create studio successfully' })
+  return res.redirect('/admin/studio')
 }
 
 module.exports = {
