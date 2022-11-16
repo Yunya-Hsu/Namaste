d46bf6513d53c93fc7b653d38e0d502f85458f30
Yunya Hsu <yunyahsu110@gmail.com>
2022-11-16 13:39:45 +0800

    feat: update admin_root controller & model at creating studio

--- models/admin_root_model.js
+++ models/admin_root_model.js
@@ -11,31 +11,36 @@ const checkSubdomain = async subdomain => {
   }
 }
 
-const findRoleIdByEmail = async email => {
-  try {
-    const [[result]] = await db.execute('SELECT role_id FROM users WHERE email = (?)', [email])
-    return result && result.role_id ? result.role_id : null
-  } catch (error) {
-    throw new Error(error)
-  }
-}
 
 
-const createStudio = async (name, introduction_title, introduction_detail, subdomain, managerRoleId, address, address_description, phone, tappay_app_key, tappay_partner_key, tappay_id, logo, introduction_photo, created_at, updated_at) => {
+const createStudio = async (name, introduction_title, introduction_detail, subdomain, managerId, address, address_description, phone, tappay_app_key, tappay_partner_key, tappay_id, tappay_app_id, logo, introduction_photo, created_at, updated_at) => {
+  const conn = await db.getConnection()
   try {
-    const [insertData] = await db.execute(
-      'INSERT INTO studios (name, introduction_title, introduction_detail, subdomain, manager, address, address_description, phone, tappay_app_key, tappay_partner_key, tappay_id, logo, introduction_photo, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-      [name, introduction_title, introduction_detail, subdomain, managerRoleId, address, address_description, phone, tappay_app_key, tappay_partner_key, tappay_id, logo, introduction_photo, created_at, updated_at]
+    await conn.beginTransaction()
+    // create studio
+    const [insertData] = await conn.execute(
+      'INSERT INTO studios (name, introduction_title, introduction_detail, subdomain, manager, address, address_description, phone, tappay_app_key, tappay_partner_key, tappay_id, tappay_app_id, logo, introduction_photo, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+      [name, introduction_title, introduction_detail, subdomain, managerId, address, address_description, phone, tappay_app_key, tappay_partner_key, tappay_id, tappay_app_id, logo, introduction_photo, created_at, updated_at]
     )
+
+    // update studio owner's permission
+    await conn.execute(
+      'INSERT INTO user_studio_role (user_id, studio_id, role_id, created_at, updated_at) VALUES (?, ?, 2, ?, ?)', // TODO: check??
+      [managerId, insertData.insertId, created_at, updated_at]
+    )
+
+    await conn.commit()
     return insertData.insertId
   } catch (error) {
+    await conn.rollback()
     throw new Error(error)
+  } finally {
+    conn.release()
   }
 }
 
 
 module.exports = {
   checkSubdomain,
-  findRoleIdByEmail,
   createStudio
 }
--- models/user_model.js
+++ models/user_model.js
@@ -3,24 +3,32 @@ const db = require('../config/mysql')
 
 
 const findUserByEmail = async email => {
-  const [[userId]] = await db.execute(
-    'SELECT id, email, password, role_id FROM users WHERE email = (?)',
-    [email]
-  )
-  return userId
+  try {
+    const [[userId]] = await db.execute(
+      'SELECT id, email, password FROM users WHERE email = (?)',
+      [email]
+    )
+    return userId
+  } catch (error) {
+    throw new Error(error)
+  }
 }
 
 const createUser = async (name, email, password, created_at, updated_at) => {
-  const [insertData] = await db.execute(
-    'INSERT INTO users (role_id, name, email, password, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?)',
-    [3, name, email, password, created_at, updated_at]
-  )
-  return insertData.insertId
+  try {
+    const [insertData] = await db.execute(
+      'INSERT INTO users (role_id, name, email, password, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?)',
+      [3, name, email, password, created_at, updated_at]
+    )
+    return insertData.insertId
+  } catch (error) {
+    throw new Error(error)
+  }
 }
 
 const deserializeUserInfo = async id => {
   const [[user]] = await db.execute(
-    'SELECT users.id, users.role_id, users.name, users.email, studios.id AS studioId, studios.subdomain FROM users LEFT JOIN studios ON users.id = studios.manager WHERE users.id = (?) ORDER BY studios.id DESC LIMIT 1',
+    'SELECT users.id, users.name, users.email, studios.id AS studioId, studios.subdomain FROM users LEFT JOIN studios ON users.id = studios.manager WHERE users.id = (?) ORDER BY studios.id DESC LIMIT 1',
     [id]
   )
   delete user.studioId
